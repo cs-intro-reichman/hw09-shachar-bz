8f68da42bdb5be9794295c9f3ea87bc7d100b1b9
shachar-bz <shachar.benzur@runi.ac.il>
2024-03-07 17:32:01 +0200

    ex9

--- List.java
+++ List.java
@@ -10,6 +10,11 @@ public class List {
 
     // The number of elements in this list
     private int size;
+
+    /** Returns the node */
+    public Node getNode() {
+        return this.first;
+    }
 	
     /** Constructs an empty list. */
     public List() {
@@ -29,40 +34,98 @@ public class List {
 
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
-        // Your code goes here
+        Node newNode = new Node(new CharData(chr));
+        newNode.next = first;
+        first = newNode;
+        this.size++;
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
-        // Your code goes here
+        if (size == 0)
+        {
+            return "()";
+        } 
+        String str = "(";
+        Node current = first;
+        while (current != null) {
+            str = str + current.cp.toString() + " ";
+            current = current.next;
+        }
+        return str.substring(0,str.length()-1)+")";
     }
 
     /** Returns the index of the first CharData object in this list
      *  that has the same chr value as the given char,
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
-        // Your code goes here
+        Node current = this.first;
+        int index = 0;
+        while (current != null) {
+            if (current.cp.equals(chr)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+
+        return -1; 
     }
 
     /** If the given character exists in one of the CharData objects in this list,
      *  increments its counter. Otherwise, adds a new CharData object with the
      *  given chr to the beginning of this list. */
     public void update(char chr) {
-        // Your code goes here
+        if (this.indexOf(chr) != -1) {
+
+            Node current = this.first;
+
+            while (current != null) {
+                if (current.cp.equals(chr)) {
+                    current.cp.count++;
+                }
+                current = current.next;
+            }
+        }
+        else {
+            this.addFirst(chr);
+        }
     }
 
     /** GIVE If the given character exists in one of the CharData objects
      *  in this list, removes this CharData object from the list and returns
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
-        // Your code goes here
+        Node pre = null;
+        Node current = first;
+        while (current != null && !current.cp.equals(chr)) {
+            pre = current;
+            current = current.next;
+        }
+        if (current == null) {
+            return false; 
+        }
+        if (pre == null) {
+            first = first.next;
+        } else {
+            pre.next = current.next;
+        }
+        size--;
+        return true;
     }
 
     /** Returns the CharData object at the specified index in this list. 
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
-        // Your code goes here
+        Node current = first;
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = 0 ; i < index ; i++) {
+            current = current.next;
+        }
+        return current.cp;
     }
 
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
@@ -80,7 +143,9 @@ public class List {
     /** Returns an iterator over the elements in this list, starting at the given index. */
     public ListIterator listIterator(int index) {
 	    // If the list is empty, there is nothing to iterate   
-	    if (size == 0) return null;
+	    if (size == 0){
+            return null;
+        }
 	    // Gets the element in position index of this list
 	    Node current = first;
 	    int i = 0;
